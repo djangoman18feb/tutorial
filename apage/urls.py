cca6bf46a7504107000b482bafc5d01c8e06dee2
djangoman18feb <djangoman18feb@gmail.com>
2018-01-11 13:33:42 -0800

    User login and authentication is complete with a simple post and quote page. holding on this project for now.....now this project is  ready for handover to a bigger project as final component

--- apage/urls.py
+++ apage/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     #/quotes/popular
     #url(r'^popular/$', views.popular_quotes, name='popular_quotes'),
 
+    #url(r'^(?P<pk>[\w-]+)/like/$', PostLikeToggle.as_view(), name='like-toggle'),
 
     #url(r'^author_detail/(?P<pk>\d+)/$', views.detail, name='author_detail'),
     url(r'^(?P<pk>[0-9]+)/$', views.Detailview.as_view(), name='author_detail'),
